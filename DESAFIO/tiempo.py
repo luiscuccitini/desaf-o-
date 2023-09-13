efe321d35d3dec3345df2d3dd4f16f454ebebc95
Axel <axel.silva.araya@gmail.com>
2023-09-13 11:50:24 -0300

    Subiendo archivos convertidor luis paredes 4A

---
+++ DESAFIO/tiempo.py
@@ -0,0 +1,13 @@
+def segundo_a_minutos(segundos):
+    return segundos /60
+
+def segundos_a_horas(segundos):
+        return segundos /3600
+
+def minutos_a_horas(minutos):
+            return minutos /60
+if __name__=="__main__":
+    segundos= 150
+    minutos= segundos_a_minutos(segundos)
+    print(f"{segundos}son equivalentes a {minutos} minutos")
+    
